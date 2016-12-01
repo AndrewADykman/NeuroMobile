4c7063f1790c91328e657ac24d15c7f228b86cce
Andrew <aadykman@gmail.com>
2016-11-30 20:09:21 -0500

    yo fuck greg

---
+++ mini_script_fuck_greg.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+import pickle
+
+with open('images.pickle','r') as f:
+  images = pickle.load(f)
+
+with open('twist.pickle','r') as g:
+  twist = pickle.load(g)
+
+shapely = np.reshape(images[0], [500, 500, 3])
+
+cv2.imshow('c', shapely)
